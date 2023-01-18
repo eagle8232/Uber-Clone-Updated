d094fa815f55ce8f8690e34406e2e9bae9c57ae6
Vusal <vnuriyev.999@gmail.com>
2023-01-19 03:53:31 +0400

    Initial Commit

---
+++ Uber Clone/Uber_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_CloneApp.swift
+//  Uber Clone
+//
+//  Created by Vusal Nuriyev on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Uber_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
